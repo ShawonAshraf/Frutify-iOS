3dd757f9d7af82097eb7deeec65bcf334fa43435
Shawon Ashraf <shawon13@live.com>
2017-12-17 00:52:41 +0600

    New app for multiclass classification

---
+++ App/Frutify Multi/Frutify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Frutify
+//
+//  Created by Shawon Ashraf on 12/10/17.
+//  Copyright © 2017 Shawon Ashraf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
